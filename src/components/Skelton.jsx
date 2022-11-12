c16a988881e88c94c2fa344591356ecd94fa831a
Irfan Deva <irfandeva@gmail.com>
2022-11-12 13:00:55 +0530

    added skelton

---
+++ src/components/Skelton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Skelton() {
+  return (
+    <div style={{ background: "red" }} className="palette">
+      <div className="ist"></div>
+      <div className="second"> </div>
+      <div className="third"> </div>
+      <div className="fourth"></div>
+    </div>
+  )
+}
+
+export default Skelton
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { lazy, Suspense, useEffect, useState } from 'react';
 import Leftbar from '../components/Leftbar';
+import Skelton from '../components/Skelton';
 //import Palette from '../components/Palette';
 import '../css/Home.css';
 
@@ -27,27 +28,11 @@ function Home() {
         <div className='grid-container'>
             {palettes.map((palette) => {
                 return (
-                    <Suspense fallback={<div>Loading...</div>}>  <Palette data={palette} key={palette.palette_id} /></Suspense>
+                    <Suspense fallback={Skelton}>  <Palette data={palette} key={palette.palette_id} /></Suspense>
                 )
             })}
 
 
-            {/* {palettes.map((palette, key) => {
-                return <div className='palette-container'>
-                    <div key={key} className="palette">
-                        <div style={{ backgroundColor: palette.color_1 }} className="ist"><p className='__color one' onClick={chooseColorr}> {palette.color_1} </p ></div>
-                        <div style={{ backgroundColor: palette.color_2 }} className="second"><p className='__color two' onClick={chooseColorr}> {palette.color_2} </p ></div>
-                        <div style={{ backgroundColor: palette.color_3 }} className="third"><p className='__color three' onClick={chooseColorr}> {palette.color_3} </p ></div>
-                        <div style={{ backgroundColor: palette.color_4 }} className="fourth"><p className='__color four' onClick={chooseColorr}> {palette.color_4} </p ></div>
-                    </div>
-
-                    <div className="footer" >
-                        <div className="likes" onClick={(e) => handleLike(e, palette.palette_id)}><img src={heart_icon_border} alt="" width={18} /> {palette.likes || 0}</div>
-                        <div className="pub-date">{palette.pub_date}</div>
-                    </div>
-                </div>
-
-            })} */}
         </div >
 
     </div>
